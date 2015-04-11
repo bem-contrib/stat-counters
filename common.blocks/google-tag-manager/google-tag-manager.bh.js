89a53b0b2ac3a2e0dfc2d739662aa825e5986d22
Alexej Yaroshevich <alex@qfox.ru>
2015-04-11 23:16:16 +0300

    Add new block: google-tag-manager
    Fixes #21

---
+++ common.blocks/google-tag-manager/google-tag-manager.bh.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = function (bh) {
+
+bh.match('google-tag-manager', function(ctx) {
+    var params = ctx.json().params,
+        scriptContent,
+        noScriptContent;
+
+    if(!params) {
+        throw Error('Missing counter parameters object');
+    }
+
+    if(!params.id) {
+        throw Error('Missing counter ID');
+    }
+
+    scriptContent = [
+        '(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':' +
+        'new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],' +
+        'j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=' +
+        '\'//www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);' +
+        '})(window,document,\'script\',\'dataLayer\',\'' + params.id + '\');'
+    ].join('\n');
+
+    noScriptContent = {
+        tag : 'iframe',
+        attrs : {
+            src : '//www.googletagmanager.com/ns.html?id=' + params.id,
+            height : '0',
+            width : '0',
+            style : 'display:none;visibility:hidden'
+        }
+    };
+
+    return [
+        {
+            tag : 'script',
+            content : scriptContent
+        },
+        {
+            tag : 'noscript',
+            content : noScriptContent
+        }
+    ];
+});
+
+};
