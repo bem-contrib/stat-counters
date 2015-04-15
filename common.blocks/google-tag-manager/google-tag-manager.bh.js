c096009c3abdb304aa91d4cf2c7037f1775c1501
Yuriy Remnev <yura_remnev@mail.ru>
2015-04-15 12:27:31 +0300

    Swapped script/noscript tags to be the same as GTM snippet

--- common.blocks/google-tag-manager/google-tag-manager.bh.js
+++ common.blocks/google-tag-manager/google-tag-manager.bh.js
@@ -16,10 +16,10 @@ bh.match('google-tag-manager', function(ctx) {
     }
 
     scriptContent = [
-        '(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':' +
-        'new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],' +
-        'j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=' +
-        '\'//www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);' +
+        '(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':',
+        'new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],',
+        'j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=',
+        '\'//www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);',
         '})(window,document,\'script\',\'dataLayer\',\'' + params.id + '\');'
     ].join('\n');
 
@@ -35,12 +35,12 @@ bh.match('google-tag-manager', function(ctx) {
 
     return [
         {
-            tag : 'script',
-            content : scriptContent
-        },
-        {
             tag : 'noscript',
             content : noScriptContent
+        },
+        {
+            tag : 'script',
+            content : scriptContent
         }
     ];
 });
